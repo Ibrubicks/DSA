d68c5310157541d27735a72c7998df5c5d24efa7
Ibrubicks <ibrubicks21@gmail.com>
2025-08-02 22:11:17 +0530

    Time: 11 ms (99.36%) | Memory: 23.1 MB (93.81%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,23 +1,21 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        total_product = 1
-        zero_count = nums.count(0)
-
-        if zero_count > 1:
-            return [0] * len(nums)
-
-        for num in nums:
-            if num != 0:
-                total_product *= num
-
         result = []
+        prod = math.prod(nums)
+        zerocount = nums.count(0)
+        prod1=1
+        if zerocount > 1:
+            return [0]*len(nums)
         for num in nums:
-            if zero_count == 1:
-                if num == 0:
-                    result.append(total_product)
-                else:
-                    result.append(0)
+            if num!=0:
+                prod1*=num
+        for num in nums:
+            if zerocount==0:
+                result.append(prod//num)
+            elif num==0:
+                result.append(prod1)
             else:
-                result.append(total_product // num)
+                result.append(0)
+
 
         return result
